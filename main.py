e4b7a7057dd8dcf96ea1321e7748d40739430b46
Osman Çalışkan <osmnclskn7@gmail.com>
2024-04-07 19:48:01 +0300

    add note-engine,pdf,prompts and main py files

---
+++ main.py
@@ -0,0 +1,45 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str, context
+from note_engine import note_engine
+from llama_index.tools import QueryEngineTool,ToolMetadata
+from llama_index.agent import ReActAgent
+from llama_index.llms import OpenAI
+from pdf import turkey_engine
+
+
+
+load_dotenv()
+
+
+population_path=os.path.join('data','population.csv')
+population_df=pd.read_csv(population_path)
+
+population_query_engine=PandasQueryEngine(df=population_df,verbose=False,instruction_str=instruction_str)
+population_query_engine.update_prompts({"pandas_prompt" : new_prompt})
+
+tools = [
+    note_engine,
+    QueryEngineTool(
+        query_engine=population_query_engine,
+        metadata=ToolMetadata(
+            name="population_data",
+            description="this gives information at the world population and demographics",
+        ),
+    ),
+    QueryEngineTool(
+        query_engine=turkey_engine,
+        metadata=ToolMetadata(
+            name="turkey_data",
+            description="this gives detailed information about turkey the country",
+        ),
+    ),
+]
+llm=OpenAI(model="gpt-3.5-turbo-0613")
+agent=ReActAgent.from_tools(tools, llm=llm,verbose=False,context=context)
+
+while (prompt := input('Enter a prompt (q to quit): ')) != "q":
+    result=agent.query(prompt)
+    print(result)
